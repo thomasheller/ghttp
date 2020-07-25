52b523655335fc3a93d2a8fd6c934ac3a8cf6b2b
Thomas Heller <thomas.m.heller@gmail.com>
2020-07-25 10:48:57 +0200

    init

---
+++ ghttp.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func JSON(req *http.Request, v interface{}) error {
+	client := http.Client{}
+	// client := http.Client{Timeout: time.Second * 10} // TODO
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+	return json.NewDecoder(res.Body).Decode(v)
+}
+
+func FormJSON(req *http.Request, data url.Values, v interface{}) error {
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Body = ioutil.NopCloser(strings.NewReader(data.Encode()))
+
+	return JSON(req, v)
+}
+
+func Success(statusCode int) bool {
+	return statusCode >= http.StatusOK && statusCode < http.StatusMultipleChoices
+}
